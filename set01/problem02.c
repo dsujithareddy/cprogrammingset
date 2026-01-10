44e79b594a847bc74e11bf457e041819a673a973
dsujithareddy <sujithadoddipalli@gmail.com>
2026-01-10 09:30:29 +0530

    Update problem02.c

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,14 @@
 //2. Write a C program to add two numbers.
-
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+clrscr();
+printf("Enter two numbers :");
+scanf("%d %d",&a,&b);
+c=a+b;
+printf("Enter two numbers : %d \n",c);
+getch();
+return 0;
+}
