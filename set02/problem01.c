22b2e9974bb725875a36445cd7a7576ade955a88
dsujithareddy <sujithadoddipalli@gmail.com>
2026-01-12 10:20:37 +0530

    Update problem01.c

---
+++ set02/problem01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void input(float *base, float *height);
+void find_area(float base , float height, float *area);
+void output(float base, float height, float area);
+int main()
+{
+float base, height, area;
+input(&base, &height);
+find_area(base, height, &area);
+output(base, height, area);
+return 0;
+}
+void input(float *base, float *height) 
+{
+printf("Enter the base of the triangle: ");
+scanf("%f", base);
+printf("Enter the height of the triangle: ");
+scanf("%f", height);
+}
+void find_area(float base, float height, float *area)
+{
+*area = (0.5) * base * height;
+}
+void output(float base, float height, float area) {
+printf("The area of the triangle with base %f and height %f is %f\n", base, height, area);
+}
