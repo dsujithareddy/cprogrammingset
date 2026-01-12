9efd7e8dd6218c5f4d2b653e328fc174e1b2753d
dsujithareddy <sujithadoddipalli@gmail.com>
2026-01-12 15:35:55 +0530

    Format code and improve readability in problem01.c

--- set02/problem01.c
+++ set02/problem01.c
@@ -4,23 +4,24 @@ void find_area(float base , float height, float *area);
 void output(float base, float height, float area);
 int main()
 {
-float base, height, area;
-input(&base, &height);
-find_area(base, height, &area);
-output(base, height, area);
+float base,height,area;
+input(&base,&height);
+find_area(base,height,&area);
+output(base,height,area);
 return 0;
 }
 void input(float *base, float *height) 
 {
 printf("Enter the base of the triangle: ");
-scanf("%f", base);
+scanf("%f",base);
 printf("Enter the height of the triangle: ");
-scanf("%f", height);
+scanf("%f",height);
 }
 void find_area(float base, float height, float *area)
 {
 *area = (0.5) * base * height;
 }
 void output(float base, float height, float area) {
-printf("The area of the triangle with base %f and height %f is %f\n", base, height, area);
+printf("The area of the triangle with base %f and height %f is %f\n",base,height,area);
 }
+
