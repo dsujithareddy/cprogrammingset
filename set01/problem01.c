919bb60ca90a386eeb72f1098107d11217b8798d
dsujithareddy <sujithadoddipalli@gmail.com>
2026-01-12 09:47:24 +0530

    Remove unnecessary headers and functions

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,10 +1,7 @@
 // 1. Write a C program to print your name.
 #include<stdio.h>
-#include<conio.h>
 int main()
 {
-  clrscr();
-printf("sujitha");
-  getch();
-  return 0;
+  printf("sujitha");
+ return 0;
 }
