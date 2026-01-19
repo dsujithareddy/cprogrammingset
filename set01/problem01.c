a1a82604fe6acd0b5d786d77ce84b39387088eeb
dsujithareddy <sujithadoddipalli@gmail.com>
2026-01-19 15:53:22 +0530

    Rewrite program to find largest of three numbers

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,7 +1,36 @@
-// 1. Write a C program to print your name.
-#include<stdio.h>
+// 1.Write a C program to find the largest of three numbers using four functions.
+#include <stdio.h>
+
+int input();
+int largest(int a, int b, int c);
+void output(int result);
 int main()
 {
-  printf("sujitha");
- return 0;
+int a, b, c;
+printf("enter the numbers");
+a = input();
+b = input();
+c = input();
+output(largest(a, b, c));
+return 0;
 }
+int input()
+{
+int n;
+scanf("%d", &n);
+return n;
+}
+int largest(int a, int b, int c)
+{
+if (a >= b && a >= c) 
+return a;
+else if (b >= a && b >= c)
+return b;
+ else 
+ return c;
+}
+void output(int result)
+{
+printf("Largest number is: %d\n", result);
+}
+
